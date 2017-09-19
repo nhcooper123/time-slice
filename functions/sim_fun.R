58e2577fb4e2f6124da06d0075ea687214fbb62c
Natalie Cooper <ncooper@tcd.ie>
2017-09-19 17:03:43 +0100

    Finish boot and rarefy all subsamples functions

--- functions/sim_fun.R
+++ functions/sim_fun.R
@@ -26,36 +26,76 @@ output.results <- function(disparity.object, bins, metric.name, inc.nodes,
   save.outputs(output, path.to.folder, slug)
 }
 
+##-------------------------------------------
+## Creating subsamples for disparity analyses
+##-------------------------------------------
 
+## Extract all types of subsample for both methods and four continuous models
+get.subsamples <- function(morphospace, tree, bins, FADLAD, inc.nodes){
 
+  # Subset samples by required number of time bins
+  subsamples.bins <- time.subsamples(data = morphospace, tree = tree, 
+  	                                 method = "discrete", time = bins, 
+  	                                 FADLAD = FADLAD, inc.nodes = inc.nodes)
+  
+  # Subset samples by required number of time slices with acctran model
+  subsamples.acctran <- time.subsamples(data = morphospace, tree = tree, 
+  	                                    method = "continuous", time = bins, 
+  	                                    FADLAD = FADLAD, inc.nodes = inc.nodes,
+                                        model = "acctran")
 
+  # Subset samples by required number of time slices with deltran model
+  subsamples.deltran <- time.subsamples(data = morphospace, tree = tree, 
+  	                                    method = "continuous", time = bins, 
+  	                                    FADLAD = FADLAD, inc.nodes = inc.nodes,
+                                        model = "deltran")
+  
+  # Subset samples by required number of time slices with punctuated model
+  subsamples.punctuated <- time.subsamples(data = morphospace, tree = tree, 
+  	                                       method = "continuous", time = bins, 
+  	                                       FADLAD = FADLAD, inc.nodes = inc.nodes,
+                                           model = "punctuated")
+  
+  # Subset samples by required number of time slices with gradual model
+  subsamples.gradual <- time.subsamples(data = morphospace, tree = tree, 
+  	                                    method = "continuous", time = bins, 
+  	                                    FADLAD = FADLAD, inc.nodes = inc.nodes,
+  	                                    model = "gradual")
 
+  return(list(subsamples.bins, subsamples.acctran, subsamples.deltran, 
+  	          subsamples.punctuated, subsamples.gradual))
 
+}
 
-
-
-
-
-# Bootstrap and rarefy
-
-boot.rarefy.data <- function(subsamples, bootstraps, metric){
+## Bootstrap and rarefy subsamples
+boot.rarefy <- function(subsamples, bootstraps){
   # Obtain a bootstrap matrix for the subsamples
   bootstrapped_data <- boot.matrix(subsamples, bootstraps = bootstraps, rarefaction = FALSE)
-  # Estimate disparity - observed and bootstrapped
+  # Obtain a rarefied bootstrap matrix for the subsamples
+  rarefied_data <- boot.matrix(subsamples, bootstraps = bootstraps, rarefaction = TRUE)
+}  
+  return(list(bootstrapped_data, rarefied_data))
+}
+
+## Bootstrap and rarefy all types of subsamples
+## subsamples is output from get.subsamples as a list
+boot.rarefy.all <- function(subsamples, bootstraps){
+  purrr::map(subsamples, boot.rarefy, bootstraps = bootstraps)
+}
+
+
+## Disparity for all
+
+    # Estimate disparity - observed and bootstrapped
   disparity <- summary(dispRity(bootstrapped_data, metric = metric), round = 10)
   
-  # Obtain a rarified bootstrap matrix for the subsamples
-  rarefied_data <- boot.matrix(subsamples, bootstraps = bootstraps, rarefaction = TRUE)
+
   # Estimate disparity - observed and bootstrapped with rarefaction
   disparity_rarefied <- summary(dispRity(rarefied_data, metric = metric), round = 10)
   
   # Return both as list
   return(list(disparity, disparity_rarefied))
 }
-
-
-  
-
 
 
 
