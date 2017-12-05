879a3d8c7406db06e59041b86ae87fa00943f587
Thomas Guillerme <guillert@tcd.ie>
2017-12-05 18:24:16 +1000

    Added individual plots for max/min plots

--- functions/figures_fun.R
+++ functions/figures_fun.R
@@ -1,26 +1,19 @@
-#' @title Plotting TreeCmp results
+#' @title Plot max min
 #'
-#' @description Simple wrapper for plotting the results from TreeCmp
+#' @description  Plotting the maximum or minimum age/disparity
 #'
-#' @param taxa_list a list of tree metrics from \code{read.TreeCmp} for all the scenario for a number of taxa
-#' @param metric a numeric of which metric to use (\code{1} = \code{MatchingCluster}, \code{2} = \code{R.F_Cluster}, \code{3} = \code{NodalSplitted}, \code{4} = \code{Triples})
-#' @param what which type of plot? Can be \code{"null"}, \code{"real"}, \code{"true"}, \code{"check"}. See details.
-#' @param ylim \code{ylim} parameter from \code{plot} (if missing is automatic)
-#' @param legend \code{logical}, whether to display the legend
-#' @param NTS \code{logical}, whether NTS was used
-#' @param ylab a \code{character} string to be passed to \code{boxplot(..., ylab)}
-#' @param xlab a \code{character} string to be passed to \code{boxplot(..., xlab)}
-#' @param axislab a \code{character} string to be passed to \code{axis(..., labels)}
-#' @param ... any additional argument to be passed to \code{boxplot}
-#'
-#' @details
-#' \code{"null"} = "maxi", "mini", "norm" tree vs. "rand"
-#' \code{"best"} = "maxi", "mini", "rand" tree vs. "norm"
-#' \code{"true"} = "norm", "rand" tree vs. "true"
-#' \code{"check"} = "norm", vs "rand"
-#' 
-#' @return
-#' A list of metrics for the comparisons.
+#' @param data the max or minimum data
+#' @param cols the different colours for the models
+#' @param lty the different line types for the CIs
+#' @param pch the point type for the median
+#' @param relative.time the relative time for scaling
+#' @param what whether to have the age on the Y axis or the disparity
+#' @param adj the size adjustment between the point
+#' @param type "age" or "epoch"
+#' @param legend whether to plot the legend
+#' @param legend.coords where to plot the legend (can be left empty)
+#' @param xaxis whether to put the labels on the x axis
+#' @param yaxis whether to put the labels on the y axis
 #' 
 #' @examples
 #' ##
@@ -29,170 +22,148 @@
 #' @export
 #' 
 
+plot.max.min <- function(data, cols = c("black", "red", "green3", "blue", "cyan", "magenta", "yellow"), lty = c(1,2), pch = 21, what = "age", adj = 0.1, type = "age", legend = TRUE, relative.time, xaxis = FALSE, yaxis = FALSE, legend.coords, ...) {
 
-plot.results.single <- function(taxa_list, metric, what, ylim, legend = FALSE, NTS = TRUE, ylab, xlab, axislab, ...) {
-    
-    ## Graphic parameters
-    ## Colours
-    cols <- c("red", "orange", "green3", "lightgreen", "blue", "lightblue")
-    
-    ## Y lab
-    if(missing(ylab)) {
-        if(metric == 1) ylab <- "Similarity (Matching Cluster)"
-        if(metric == 2) ylab <- "Similarity (Robinson-Foulds)"
-        if(metric == 3) ylab <- "Similarity (Nodal Split)"
-        if(metric == 4) ylab <- "Similarity (Triplets)"
-    }
-
-    ## Y lim
-    get.min.list <- function(X, metric) {
-        return(min(unlist(X[,metric])))
-    }
-
-    min <- min(unlist(lapply(unlist(unlist(unlist(taxa_list, recursive = FALSE), recursive = FALSE), recursive = FALSE), get.min.list, metric)))
-    if(min < 0) {
-        ylim <- c(min,1)
+    ## select the data type
+    if(type == "age") {
+        data <- data[1:21, ]
     } else {
-        ylim <- c(0,1)
+        data <- data[22:42, ]
     }
 
-    ## X lab
-    if(missing(xlab)) {
-        xlab <- "Characters"
+    ## Get the type of data
+    if(what == "age") {
+        make.numeric <- function(X) {
+            options(warn = -1)
+            num_X <- as.numeric(X)
+            options(warn = 0)
+            if(is.na(num_X)) {
+                return(as.numeric(strsplit(X, split = " - ")[[1]]))
+            } else {
+                return(num_X)
+            }
+        }
+        ## Convert ages into numeric values
+        data_values <- lapply(as.list(data[,1]), make.numeric)
+
+        ## Adjusting ages
+        if(!missing(relative.time)) {
+            data_values <- lapply(data_values, function(X, relative.time) return(X/relative.time), relative.time)
+            ylim <- c(0,1)
+        } else {
+            ylim <- range(unlist(data_values))
+        }
+
+    } else {
+        data_values <- data[,4:8]
+        ylim <- range(as.vector(data_values))
     }
 
-    ## axis lab
-    if(missing(axislab)) {
-        axislab <- c("100", "350", "1000")
+    ## Get the sorting
+    # data_method <- list(which(data$bin_type == "stratigraphy"), which(data$bin_type == "duration"), which(data$bin_type == "number"))
+
+    ## Plot parameters
+    xlim <- c(0, 3)
+
+    ## Empty plot
+    ylab <- ifelse(yaxis, what, "")
+    plot(0,0, col = "white", xlim = xlim, ylim = ylim, xaxt = "n", yaxt = "n", xlab = "", ylab = ylab, ...)
+    #plot(0,0, col = "white", xlim = xlim, ylim = ylim, xaxt = "n", yaxt = "n", xlab = "", ylab = ylab)
+    ## Add the lines
+    abline(v = c(1, 2), lty = 3)
+    ## Add the x axis
+    if(xaxis) axis(1, at = c(0.5, 1.5, 2.5), label = c("stratigraphy", "duration", "number"), tick = FALSE)
+    ## Add the y axis
+    if(yaxis) axis(2)
+
+    plot.point.line <- function(y_coordinates, x_loc, col, lty, pch) {
+        if(length(y_coordinates) > 1) {
+            lines(x = rep(xloc, 2), y_coordinates, lty = lty, col = col, lwd = 5)
+        } else {
+            points(x = xloc, y = y_coordinates, pch = pch, col = col)
+        }
     }
 
-    ## Horizontal line
-    line <- 0
+    ## Number of models
+    n_models <- length(unique(data$model))
     
+    ## Add the data
+    if(what == "age") {
+        ## Stratigraphy
+        xloc <- adj
+        for(model in 1:n_models) {
+            ## Plot the age
+            plot.point.line(data_values[[model]], xloc, col = cols[model], lty = lty, pch = pch)
+            ## Increment xloc
+            xloc <- xloc + adj
+        }
 
-    if(what == "null") {
-        boxplot(xlab = xlab, ylab = ylab, xaxt = "n", ylim = ylim, col = rep(cols, 3), las = 1, ...,
-            ## c100
-            unlist(taxa_list$c100$bayesian$rand$maxi[,metric]), unlist(taxa_list$c100$parsimony$rand$maxi[,metric]),
-            unlist(taxa_list$c100$bayesian$rand$mini[,metric]), unlist(taxa_list$c100$parsimony$rand$mini[,metric]),
-            unlist(taxa_list$c100$bayesian$rand$norm[,metric]), unlist(taxa_list$c100$parsimony$rand$norm[,metric]),
-            ## c350
-            unlist(taxa_list$c350$bayesian$rand$maxi[,metric]), unlist(taxa_list$c350$parsimony$rand$maxi[,metric]),
-            unlist(taxa_list$c350$bayesian$rand$mini[,metric]), unlist(taxa_list$c350$parsimony$rand$mini[,metric]),
-            unlist(taxa_list$c350$bayesian$rand$norm[,metric]), unlist(taxa_list$c350$parsimony$rand$norm[,metric]),
-            ## c1000
-            unlist(taxa_list$c1000$bayesian$rand$maxi[,metric]), unlist(taxa_list$c1000$parsimony$rand$maxi[,metric]),
-            unlist(taxa_list$c1000$bayesian$rand$mini[,metric]), unlist(taxa_list$c1000$parsimony$rand$mini[,metric]),
-            unlist(taxa_list$c1000$bayesian$rand$norm[,metric]), unlist(taxa_list$c1000$parsimony$rand$norm[,metric])
-            )
+        ## Duration
+        xloc <- adj + 1
+        for(model in 1:n_models) {
+            ## Plot the age
+            plot.point.line(data_values[[model+n_models]], xloc, col = cols[model], lty = lty, pch = pch)
+            ## Increment xloc
+            xloc <- xloc + adj
+        }
 
-        ## X axis
-        axis(1, 1:18, labels = FALSE, tick = FALSE)
-        axis(1, c(3.5, 9.5, 15.5), tick = FALSE, labels = axislab)
-        ## Lines
-        abline(v = 6.5) ; abline(v = 12.5)
-        abline(h = line, col = "grey", lty = 2)
-        ## Legend
-        if(legend) {
-            legend("bottomleft", col = cols, pch = 19, bty = "n", title = "Character differences:", cex = 0.8,
-                legend = c("Maximised (Bayesian)", "Maximised (Parsimony)",
-                           "Minimised (Bayesian)", "Minimised (Parsimony)",
-                           "Normal (Bayesian)", "Normal (Parsimony)")
-                )
+        ## Number
+        xloc <- adj + 2
+        for(model in 1:n_models) {
+            ## Plot the age
+            plot.point.line(data_values[[model+n_models*2]], xloc, col = cols[model], lty = lty, pch = pch)
+            ## Increment xloc
+            xloc <- xloc + adj
+        }
+    } else {
+
+        ## Stratigraphy
+        xloc <- adj
+        for(model in 1:n_models) {
+            ## Plot the age
+            points(x = xloc, y = data_values$bs.median[model], pch = pch, col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,3][model], data_values[,4][model]), lty = lty[1], col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,2][model], data_values[,5][model]), lty = lty[2], col = cols[model])
+            ## Increment xloc
+            xloc <- xloc + adj
+        }
+
+        ## Duration
+        xloc <- adj + 1
+        for(model in 1:n_models) {
+            ## Plot the age
+            points(x = xloc, y = data_values$bs.median[model], pch = pch, col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,3][model+n_models], data_values[,4][model]), lty = lty[1], col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,2][model+n_models], data_values[,5][model]), lty = lty[2], col = cols[model])
+            ## Increment xloc
+            xloc <- xloc + adj
+        }
+
+        ## Number
+        xloc <- adj + 2
+        for(model in 1:n_models) {
+            ## Plot the age
+            points(x = xloc, y = data_values$bs.median[model], pch = pch, col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,3][model+n_models*2], data_values[,4][model]), lty = lty[1], col = cols[model])
+            lines(x = rep(xloc, 2), y = c(data_values[,2][model+n_models*2], data_values[,5][model]), lty = lty[2], col = cols[model])
+            ## Increment xloc
+            xloc <- xloc + adj
         }
     }
 
-    if(what == "best") {
-        boxplot(xlab = xlab, ylab = ylab, xaxt = "n", ylim = ylim, col = rep(cols, 3), las = 1, ...,
-            ## c100
-            unlist(taxa_list$c100$bayesian$norm$maxi[,metric]), unlist(taxa_list$c100$parsimony$norm$maxi[,metric]),
-            unlist(taxa_list$c100$bayesian$norm$mini[,metric]), unlist(taxa_list$c100$parsimony$norm$mini[,metric]),
-            unlist(taxa_list$c100$bayesian$norm$rand[,metric]), unlist(taxa_list$c100$parsimony$norm$rand[,metric]),
-            ## c350
-            unlist(taxa_list$c350$bayesian$norm$maxi[,metric]), unlist(taxa_list$c350$parsimony$norm$maxi[,metric]),
-            unlist(taxa_list$c350$bayesian$norm$mini[,metric]), unlist(taxa_list$c350$parsimony$norm$mini[,metric]),
-            unlist(taxa_list$c350$bayesian$norm$rand[,metric]), unlist(taxa_list$c350$parsimony$norm$rand[,metric]),
-            ## c1000
-            unlist(taxa_list$c1000$bayesian$norm$maxi[,metric]), unlist(taxa_list$c1000$parsimony$norm$maxi[,metric]),
-            unlist(taxa_list$c1000$bayesian$norm$mini[,metric]), unlist(taxa_list$c1000$parsimony$norm$mini[,metric]),
-            unlist(taxa_list$c1000$bayesian$norm$rand[,metric]), unlist(taxa_list$c1000$parsimony$norm$rand[,metric])
-            )
-
-        ## X axis
-        axis(1, 1:18, labels = FALSE, tick = FALSE)
-        axis(1, c(3.5, 9.5, 15.5), tick = FALSE, labels = axislab)
-        ## Lines
-        abline(v = 6.5) ; abline(v = 12.5)
-        abline(h = line, col = "grey", lty = 2)
-        ## Legend
-        if(legend) {
-            legend("bottomleft", col = cols, pch = 19, bty = "n", title = "Character differences:", cex = 0.8,
-                legend = c("Maximised (Bayesian)", "Maximised (Parsimony)",
-                           "Minimised (Bayesian)", "Minimised (Parsimony)",
-                           "Randomised (Bayesian)", "Randomised (Parsimony)")
-                )
+    ## Add the legend
+    if(legend) {
+        if(missing(legend.coords)) {
+            legend.coords <- "topleft"
         }
-    }
-
-    if(what == "true") {
-        boxplot(xlab = xlab, ylab = ylab, xaxt = "n", ylim = ylim, col = rep(cols[1:4], 3), las = 1, ...,
-            ## c100
-            unlist(taxa_list$c100$bayesian$norm$true[,metric]), unlist(taxa_list$c100$parsimony$norm$true[,metric]),
-            unlist(taxa_list$c100$bayesian$rand$true[,metric]), unlist(taxa_list$c100$parsimony$rand$true[,metric]),
-            ## c350
-            unlist(taxa_list$c350$bayesian$norm$true[,metric]), unlist(taxa_list$c350$parsimony$norm$true[,metric]),
-            unlist(taxa_list$c350$bayesian$rand$true[,metric]), unlist(taxa_list$c350$parsimony$rand$true[,metric]),
-            ## c1000
-            unlist(taxa_list$c1000$bayesian$norm$true[,metric]), unlist(taxa_list$c1000$parsimony$norm$true[,metric]),
-            unlist(taxa_list$c1000$bayesian$rand$true[,metric]), unlist(taxa_list$c1000$parsimony$rand$true[,metric])
-            )
-
-        ## X axis
-        axis(1, 1:12, labels = FALSE, tick = FALSE)
-        axis(1, c(2.5, 6.5, 10.5), tick = FALSE, labels = axislab)
-        ## Lines
-        abline(v = 4.5) ; abline(v = 8.5)
-        abline(h = line, col = "grey", lty = 2)
-        ## Legend
-        if(legend) {
-            legend("bottomleft", col = cols[1:4], pch = 19, bty = "n", title = "Character differences:", cex = 0.8,
-                legend = c("Normal (Bayesian)", "Normal (Parsimony)",
-                           "Randomised (Bayesian)", "Randomised (Parsimony)")
-                )
-        }
-    }
-
-    if(what == "check") {
-        boxplot(xlab = xlab, ylab = ylab, xaxt = "n", ylim = ylim, col = rep(cols[1:4], 3), las = 1, ...,
-            ## c100
-            unlist(taxa_list$c100$bayesian$norm$rand[,metric]), unlist(taxa_list$c100$parsimony$norm$rand[,metric]),
-            unlist(taxa_list$c100$bayesian$rand$norm[,metric]), unlist(taxa_list$c100$parsimony$rand$norm[,metric]),
-            ## c350
-            unlist(taxa_list$c350$bayesian$norm$rand[,metric]), unlist(taxa_list$c350$parsimony$norm$rand[,metric]),
-            unlist(taxa_list$c350$bayesian$rand$norm[,metric]), unlist(taxa_list$c350$parsimony$rand$norm[,metric]),
-            ## c1000
-            unlist(taxa_list$c1000$bayesian$norm$rand[,metric]), unlist(taxa_list$c1000$parsimony$norm$rand[,metric]),
-            unlist(taxa_list$c1000$bayesian$rand$norm[,metric]), unlist(taxa_list$c1000$parsimony$rand$norm[,metric])
-            )
-
-        ## X axis
-        axis(1, 1:12, labels = FALSE, tick = FALSE)
-        axis(1, c(2.5, 6.5, 10.5), tick = FALSE, labels = axislab)
-        ## Lines
-        abline(v = 4.5) ; abline(v = 8.5)
-        abline(h = line, col = "grey", lty = 2)
-        ## Legend
-        if(legend) {
-            legend("bottomleft", col = cols[1:4], pch = 19, bty = "n", title = "Character differences:", cex = 0.8,
-                legend = c("Normal (Bayesian)", "Normal (Parsimony)",
-                           "Randomised (Bayesian)", "Randomised (Parsimony)")
-                )
-        }
+        legend(legend.coords, col = cols[1:n_models], pch = pch, bty = "n", cex =0.8, legend = unique(data$model))
     }
 }
 
 
-
+multi.plot.max.min <- function() {
+    return(NULL)
+}
 
 #' @title Plotting extinction results
 #'
